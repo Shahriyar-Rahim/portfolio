4905e201c566bebf0f5f4905f13e0ccf32651e3e
Md. Shahriyar Rahim <rahimislam420.ri35@gmail.com>
2026-07-11 18:12:27 +0600

    added vercel config

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./configs/database.config.js";
 import dns from "dns";
-
-// Routes
 import userRoutes from "./routes/user.routes.js";
 import commentRoutes from "./routes/comment.route.js";
 import educationRoutes from "./routes/education.routes.js";
@@ -29,8 +27,8 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
+connectDB();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
@@ -46,7 +44,7 @@ app.use(
         return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS"));
+      return callback(null, true);
     },
     credentials: true,
   }),
@@ -55,11 +53,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-const frontendPath = path.join(__dirname, "../frontend/dist");
-app.use(express.static(frontendPath));
-
-connectDB();
 
 app.use("/api/v1/auth", authLimiter, userRoutes);
 app.use("/api/v1/comment", commentRoutes);
@@ -77,14 +70,9 @@ app.use("/api/v1/portfolio/github", githubRoutes);
 app.use("/api/v1/auth/recovery", authRecoveryRoutes);
 app.use("/api/v1/about", aboutRoutes);
 
-app.use((req, res) => {
-  if (req.originalUrl.startsWith("/api")) {
-    return res.status(404).json({ message: "API route not found" });
-  }
-
-  res.sendFile(path.join(frontendPath, "index.html"));
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
-
 
 app.use((err, req, res, next) => {
   console.error("ERROR:", err.message);
@@ -94,6 +82,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default app;
